26203a9ed93eabfecb10d421f14a42e6dafe1191
krzysztof_krawczyk <krzysztof.krawczyk@intive.com>
2018-02-23 14:29:34 +0100

    Added instance id

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
 var counter = 0
-app.get('/', (req, res) => res.send('Hello World! Nb of visits is ' + ++counter))
+var id = Math.floor(Math.random()*100)
+app.get('/', (req, res) => res.send('Hello World! Nb of visits is ' + ++counter + ' from instance ' + id))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
